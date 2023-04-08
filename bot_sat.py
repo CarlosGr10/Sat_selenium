38b5c42fc4da5b1f1e0008549c0eb9491950594c
CarlosGr10 <tigrecarlos1@gmail.com>
2023-04-08 11:03:01 -0600

    fill dates

--- bot_sat.py
+++ bot_sat.py
@@ -6,6 +6,7 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import Select
+from selenium.common.exceptions import ElementNotInteractableException
 import os
 import time
 
@@ -79,8 +80,7 @@ def page_web():
     # Tab Datos del complemento
     WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, "//*[@id=\"tabs\"]/ul/li[5]"))).click()
 
-    time.sleep(2)
-
+    time.sleep(1)
 
     # Adicionar pagos
     button_adiciona_pago = driver.find_element("xpath", "//*[@id=\"btnAddPago\"]")
@@ -90,14 +90,25 @@ def page_web():
     fecha_pago.send_keys("2023-03-16 12:00:00")
     fecha_pago.send_keys(Keys.ENTER)
 
+    time.sleep(1)
+    
     # Ciclo Impuestos P. Traslados
-    button_impuesto_traslado = driver.find_element("xpath", "//*[@id=\"btnAddTrasladoP\"]")
-    button_impuesto_traslado.click()
+    driver.implicitly_wait(10)
+    wait = WebDriverWait(driver, 10)
+    button_impuesto_traslado = wait.until(EC.element_to_be_clickable((By.ID, 'btnAddTrasladoP')))
+    attempts = 0
+    while attempts < 5:
+        try:
+            button_impuesto_traslado.click()
+            break
+        except ElementNotInteractableException:
+            attempts += 1
 
     driver.implicitly_wait(10)
-    base = driver.finrfc = driver.find_element("id", "TrasladoP_BaseP")
-    base.send_keys("1")
+    base = driver.finrfc = driver.find_element(By.XPATH, '//*[@id="TrasladoP_BaseP"]')
+    base.send_keys(1)
     base.send_keys(Keys.ENTER)
+   
 
     driver.implicitly_wait(10)
     impuesto_p = driver.find_element(By.XPATH, "//*[@id=\"TrasladoP_ImpuestoP\"]")
@@ -120,9 +131,55 @@ def page_web():
     wait = WebDriverWait(driver, 10)
     button_adicionar_documentos = driver.find_element("xpath", "//*[@id=\"btnAddDoc\"]")
     button_adicionar_documentos.click()
-   
 
-    time.sleep(10)
+    id_doc = driver.finrfc = driver.find_element("xpath",'//*[@id="txtPDoc_IdDocumento"]')
+    id_doc.send_keys("HOLAAAAAA")
+    id_doc.send_keys(Keys.ENTER)
+    
+    serie = driver.finrfc = driver.find_element("xpath",'//*[@id="txtPDoc_Serie"]')
+    serie.send_keys("A")
+    serie.send_keys(Keys.ENTER)
+    
+    folio = driver.finrfc = driver.find_element("xpath",'//*[@id="txtPDoc_Folio"]')
+    folio.send_keys("B")
+    folio.send_keys(Keys.ENTER)
+    
+    equivalencia = driver.finrfc = driver.find_element("xpath",'//*[@id="txtPDoc_TipoCambioDR"]')
+    equivalencia.send_keys("1")
+    equivalencia.send_keys(Keys.ENTER)
+    
+    num_parcialidad = driver.finrfc = driver.find_element("xpath",'//*[@id="txtPDoc_NumParcialidad"]')
+    num_parcialidad.send_keys("2")
+    num_parcialidad.send_keys(Keys.ENTER)
+    
+    sueldo_anterior = driver.finrfc = driver.find_element("xpath",'//*[@id="txtPDoc_ImpSaldoAnt"]')
+    sueldo_anterior.send_keys("3")
+    sueldo_anterior.send_keys(Keys.ENTER)
+    
+    imp_pagado = driver.finrfc = driver.find_element("xpath",'//*[@id="txtPDoc_ImpPagado"]')
+    imp_pagado.send_keys("4")
+    imp_pagado.send_keys(Keys.ENTER)
+    
+    sueldo_insoluto = driver.finrfc = driver.find_element("xpath",'//*[@id="txtPDoc_ImpSaldoInsoluto"]')
+    sueldo_insoluto.send_keys("5")
+    sueldo_insoluto.send_keys(Keys.ENTER)
+
+    driver.implicitly_wait(10)
+    moneda = driver.find_element(By.XPATH, '//*[@id="ddlPDoc_MonedaDR"]')
+    select = Select(moneda)
+    select.select_by_value("MXN|2")
+
+    driver.implicitly_wait(10)
+    obj_im_dr = driver.find_element(By.XPATH, '//*[@id="ddlPDoc_ObjetoImpDR"]')
+    select = Select(obj_im_dr)
+    select.select_by_value("03")
+
+    buttom_adicion = guardar_button = element.find_element("xpath", '//*[@id="btnAddOkDoc"]')
+    buttom_adicion.click()
+
+    
+    
+    time.sleep(2)
 
 
     '''
